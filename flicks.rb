bc93c1fc75051373bc0e2184827822e5966d90f9
Rob Colby <rcolby@me.com>
2013-04-09 11:28:07 -0700

    Still working on it

--- flicks.rb
+++ flicks.rb
@@ -1,48 +1,60 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/reloader'
 require 'better_errors'
 require 'json'
 require 'uri'
 require 'open-uri'
 
-class Flicks < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-      use BetterErrors::Middleware
-      BetterErrors.application_root = File.expand_path("..", __FILE__)
-  end
 
-  configure do
-    @app_name = "Flicks"
-  end
+configure :development do
+  register Sinatra::Reloader
+    use BetterErrors::Middleware
+    BetterErrors.application_root = File.expand_path("..", __FILE__)
+end
 
-  before do
-    @app_name = "Flicks"
-    @page_title = @app_name.dup
-  end
+configure do
+  @app_name = "Flicks"
+end
 
-  get '/' do
-    @page_title += ": Home"
-    erb :home
-  end
+before do
+  @app_name = "Flicks"
+  @page_title = @app_name.dup
+end
 
-  get '/search' do
-    @query = params[:q]
-    @button = params[:button]
-    file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
-    @results = JSON.load(file.read)["Search"] || []
-    erb :results
-  end
+get '/' do
+  @page_title += ": Home"
+  erb :home
+end
 
-  get '/movies' do
-    @id = params[:id]
-    file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
+get '/search' do
+  @query = params[:q]
+  @page_title += "Search Results for #{@query}"
+  @button = params[:button]
+  if @button == "lucky"
+    file = open("http://www.omdbapi.com/?t=#{URI.escape(@query)}")
     @result = JSON.load(file.read)
     @actors = @result["Actors"].split(", ")
     @directors = @result["Director"]
     erb :detail
-
+  else
+    file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
+    @results = JSON.load(file.read)["Search"] || []
   end
-
-  run!
+  if @results.size == 1
+    redirect "/movies?id=#{@results.first["imdbID"]}"
+  else
+    erb :results
+  end
 end
+
+get '/movies' do
+  @id = params[:id]
+  file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
+  @result = JSON.load(file.read)
+  @page_title += ": "
+  @actors = @result["Actors"].split(", ")
+  @directors = @result["Director"].split(", ")
+  erb :detail
+
+end
+
